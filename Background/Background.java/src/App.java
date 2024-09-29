0b5e0093afedf22ec9f9a98fea81fc0f070d2b65
g24m3630 <Shimanetshephang19@gmail.com>
2024-09-29 17:45:01 +0200

    Add files via upload

--- Background/Background.java/src/App.java
+++ Background/Background.java/src/App.java
@@ -1,23 +1,27 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.Color;
+import java.awt.Dimension;
+
 import javax.swing.*;
-public class App {
+public class App extends JPanel {
     JFrame frame = new JFrame("FLAPPY BIRD 💕❤️🦜🦜🦜");
     JButton button;
     App(){
+       
         button  = new JButton();
         button.setBounds(220,230,140,80);
         button.setText("PLAY");
-        
-        frame.setTitle("Floppy");
-        frame.setSize(800,1360);
-        frame.setResizable(false);
-        frame.setLocationRelativeTo(null);
+      
+      
+        frame.setSize(900,900);  
+        frame.setVisible(true); 
+        //frame.setVisible(true);
         frame.add(button);
         frame.setLayout(null);
+        frame.setResizable(false);
 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+        
         Title title = new Title();
         button.addActionListener(title);
     }
@@ -27,15 +31,17 @@ public class App {
     public void Gamescreen(){
         JFrame frame = new JFrame("FLAPPY BIRD 💕❤️🦜🦜🦜");
         button.setVisible(false);
+
         Background background= new Background();
-        frame.add(background);  
-        frame.setSize(600,600);  
+       frame.add(background);  
+       frame.setSize(600,600);  
        frame.setVisible(true); 
         frame.add(background); 
         frame.pack();
         background.requestFocus();
        frame.setVisible(true);
-      
+       frame.setResizable(false);
+     
     }
 
    public class Title implements ActionListener{
--- Background/Background.java/src/Background.java
+++ Background/Background.java/src/Background.java
@@ -9,7 +9,7 @@ import javax.swing.*;
 
 public class Background extends JPanel implements ActionListener,KeyListener {
     int height = 750;
-    int width = 1360;
+    int width = 1260;
 
     JFrame frame = new JFrame("Flappy Bird 💕❤️🦜🦜🦜");
     Image Top;
@@ -18,16 +18,70 @@ public class Background extends JPanel implements ActionListener,KeyListener {
     Image bot;
     Image ice;
     Image golds;
+    Image bronzes;
+    Image silvers;
+    Image platinums ;
     //instances for the bird...
     int birdX=width/6;
     int birdY=height/2;
     int birdwidth = 40;
     int birdheight = 30;
+    //instances for the bronze..
+    int bronzeX=70;
+    int bronzeY=30;
+    int bronzewidth = 40;
+    int bronzeheight = 30;
+    class Bronze{
+      int Xb = bronzeX;
+      int Yb = bronzeY;
+      int bswidth = bronzewidth;
+      int bsheight = bronzeheight;
+      Image zmge;
+      Bronze(Image zmge){
+        this.zmge = zmge;
+    }
+  }
+  //Instances for the Silver
+  int silverX=20;
+  int silverY=30;
+  int silverwidth = 40;
+  int silverheight = 30;
+ 
+class Silver{
+    int Xs = silverX;
+    int Ys = silverY;
+    int swidth = silverwidth;
+    int sheight = silverheight;
+    Image smge;
+    Silver(Image smge){
+      this.smge = smge;
+  }
+}
+  //Instances for the Platinum
+  int platinumX=20;
+  int platinumY=30;
+  int platinumwidth = 40;
+  int platinumheight = 30;
+ 
+class Platinum{
+    int Xm = platinumX;
+    int Ym = platinumY;
+    int mwidth = platinumwidth;
+    int mheight = platinumheight;
+    Image pmge;
+    Platinum(Image pmge){
+      this.pmge = pmge;
+  }
+}
+
+
     //instances for the gold...
+
     int goldX=20;
     int goldY=30;
     int goldwidth = 40;
     int goldheight = 30;
+   
 class Gold{
       int Xg = goldX;
       int Yg = goldY;
@@ -72,8 +126,11 @@ class Gold{
    private  int speedP=-9;      //speed for the pipe....
   private  ArrayList<Pipe> pipes; ///list for the pipe...
     
-   public static  Bird bird;
+  public static  Platinum platinum; 
+  public static  Silver silver;
+  public static  Bird bird;
    private  static  Gold gold;
+   private  static  Bronze bronze;
     public Random rand =new Random();
     private Timer time;
    public  boolean gameOver= false;
@@ -81,9 +138,10 @@ class Gold{
    public  int speed = 0; /// speed of the bird...
     int opp = 1;           ///speed of the bird opposed downwards 
     double score = 0.0;
+    int timerScore;
     Background() {   //constructer 
         setPreferredSize(new Dimension(width,height));
-        setBackground(Color.white);
+        setBackground(Color.pink);
         setFocusable(true);
         addKeyListener(this);
        Top = new ImageIcon(getClass().getResource("./tube.png")).getImage();
@@ -92,12 +150,20 @@ class Gold{
         bot = new ImageIcon(getClass().getResource("./iceBackground.jpg")).getImage();
         ice = new ImageIcon(getClass().getResource("./try.png")).getImage(); 
         golds = new ImageIcon(getClass().getResource("./gold.png")).getImage();
+        bronzes = new ImageIcon(getClass().getResource("./bronze.png")).getImage();
+        silvers = new ImageIcon(getClass().getResource("./Silver.png")).getImage();
+        platinums = new ImageIcon(getClass().getResource("./Platinum.png")).getImage();
+        platinum = new Platinum(platinums);
+        silver = new Silver(silvers);
         bird = new Bird(birds);
         gold=new Gold(golds);
+        bronze=new Bronze(bronzes);
       time = new Timer(1000/60,this);
       pipes = new ArrayList<>();
+
       pipesTimer = new Timer(1500,new ActionListener() {
         @Override
+        
 	public void actionPerformed(ActionEvent e){
       pipeimport();
       repaint();
@@ -105,7 +171,7 @@ class Gold{
     
       });
     
-      pipesTimer.start();              
+      pipesTimer.start(); 
       time.start();       
     }
     ////importing the pipes.....
@@ -133,12 +199,14 @@ class Gold{
       bird.Y = Math.max(bird.Y,0);
       int i =0;
       while(i < pipes.size()){
+
         Pipe pipe= pipes.get(i);
         pipe.Xp += speedP;
         i++;
-        if(!pipe.succ && bird.X > pipe.Yp + pipewidth){
+        if(!pipe.succ && bird.X > pipe.Yp + pipewidth ){
           pipe.succ = true;
           score += 0.5;
+         timerScore = (int)score*3;
 
         }
         if(Intersection(bird,pipe)){
@@ -171,34 +239,36 @@ class Gold{
 
     g.setColor(Color.darkGray );
     g.setFont(new Font("Arial",Font.PLAIN, 32));
-
+    
     if(gameOver){
      g.drawImage(ice, 0, 0, width, height, null);
       pipesTimer.stop();
+    
       time.stop();
-      g.drawString("  SCORE : "+String.valueOf((int) score) , 100 , 300);
+      g.drawString("  Score : "+String.valueOf((int) score) , 100 , 300);
          g.drawString("  CLICK TO START ", 100 , 350);
          
     }
     else{
-      g.drawString("  SCORE : "+String.valueOf((int) score) , 10, 30);
+      g.drawString("  Score : "+String.valueOf((int) score) , 10, 30);
+      g.drawString("  P--> PAUSE   " , 10, 100);
     }
     if(score >=10){
       speedP =-13;
-      score +=0.01;
+     // score +=0.01;
     }
     if(score >=10){
-      g.drawImage(gold.gmge, gold.Xg, gold.Yg, gold.gwidth, gold.gheight, null);
+      g.drawImage(bronze.zmge, bronze.Xb - 40 , bronze.Yb, bronze.bswidth, bronze.bsheight, null);
   
     }
+    if(score >=50){
+      g.drawImage(silver.smge, silver.Xs + 40, silver.Ys  , silver.swidth, silver.sheight, null);
+    }
     if(score >=100){
-      g.drawImage(gold.gmge, gold.Xg + 40, gold.Yg  , gold.gwidth, gold.gheight, null);
+      g.drawImage(gold.gmge, gold.Xg + 80, gold.Yg, gold.gwidth, gold.gheight, null);
     }
-    if(score >=1000){
-      g.drawImage(gold.gmge, gold.Xg + 40, gold.Yg, gold.gwidth, gold.gheight, null);
-    }
-    if(score >=2000){
-      g.drawImage(gold.gmge, gold.Xg + 40, gold.Yg ,  gold.gwidth, gold.gheight, null);
+    if(score >=200){
+      g.drawImage(platinum.pmge, platinum.Xm + 120, platinum.Ym ,  platinum.mwidth, platinum.mheight, null);
     }
     }
 	@Override
@@ -215,12 +285,22 @@ class Gold{
 	@Override
 	public void keyTyped(KeyEvent e) {
 		/////////////////////////////////
+  
 	}
 	@Override
 	public void keyPressed(KeyEvent e) {
+    if(e.getKeyCode()== KeyEvent.VK_BACK_SLASH){
+      pipesTimer.stop();
+      time.stop();
+    }
+    if(e.getKeyCode()== KeyEvent.VK_0){
+      pipesTimer.start();
+      time.start();
+    }
 	if(e.getKeyCode() == KeyEvent.VK_SPACE){
     speed = -9;
   }	
+  
   if(gameOver){
     bird.Y=birdY;
     pipes.clear();
