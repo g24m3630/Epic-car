74d26d256621e22842a12eedc74c457c4a88319e
Ripzin27 <g24n9666@campus.ru.ac.za>
2024-10-06 15:35:14 +0200

    Update Music.java

--- Music.java
+++ Music.java
@@ -1,9 +1,23 @@
 import java.io.File;
+
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.swing.JOptionPane;
-class Music{
+public class Music{
+ // private String wgvd;
+
+  // public Music(String path){
+  //   this.wgvd = path;
+  // }
+
+  public static void play(String args){
+    String filepath =args;
+    PlayMusic(filepath);
+    
+    
+  }
+
 
 public static void PlayMusic(String pathway ){
     try{
@@ -15,6 +29,7 @@ public static void PlayMusic(String pathway ){
             sample.open(audioInput); //opening the cd player and inserting the music 
             int c = 0;
             int s = 5;
+            JOptionPane.showMessageDialog(null,"It works perfectly");
             while(c<s){
                  sample.start();// starts playing the music
           }
