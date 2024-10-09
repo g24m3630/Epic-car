bb4b15954c73f0bba36de9d7fa13f909cef10028
g24m3630 <Shimanetshephang19@gmail.com>
2024-10-09 21:12:27 +0200

    Update Musico.java

--- Background/Background.java/src/Musico.java
+++ Background/Background.java/src/Musico.java
@@ -1,36 +1,26 @@
+
 import java.io.File;
 import java.io.IOException;
-
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
+public class Sound {
+    public static void Runmusic(String path) throws LineUnavailableException{
+        try {
+            AudioInputStream input=AudioSystem.getAudioInputStream(new File(path));
+            Clip clip=AudioSystem.getClip();
+            clip.open(input);
+            clip.loop(5);
 
- 
-public class Musico {
-    public static void Run(String pathway,int c){
-        try{
-            AudioInputStream audioInput = AudioSystem.getAudioInputStream(new File(pathway)); //gets the music at the file path
-            Clip sample = AudioSystem.getClip();//essentally the music player 
-            sample.open(audioInput); //opening the cd player and inserting the music
-            int s=1;
-            while (c<s) {
-
-            sample.start();
-        }
-
-    }
-        catch(LineUnavailableException e){
+        } catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        }catch (IOException e) {
             e.printStackTrace();
         }
-        catch(UnsupportedAudioFileException e){
+        catch (LineUnavailableException e) {
             e.printStackTrace();
         }
-        catch(IOException e){
-            e.printStackTrace();
-        }
-    }
-
-    
+}
 }
